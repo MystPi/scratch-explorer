f059acb057621fb3e5fbdd25c6600b8311a7ff2d
MystPi <86574651+MystPi@users.noreply.github.com>
2022-04-28 19:08:41 -0400

    fix: eliminate case-dependant alphabetical sorting

--- src/lib/services.js
+++ src/lib/services.js
@@ -218,8 +218,8 @@ const services = [
 
 function sortAlphabetically(services) {
   return services.sort((a, b) => {
-    if (a.title < b.title) return -1;
-    if (a.title > b.title) return 1;
+    if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+    if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
     return 0;
   });
 }
